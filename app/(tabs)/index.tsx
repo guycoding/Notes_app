f191ac1901dc6c2bee976bfcab614886dad95bc0
guycoding <charlesmouzen@gmail.com>
2025-07-17 22:42:33 +0200

    types folder
    type folder added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,20 @@ import { useAuth } from "@/lib/auth-context";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import { Button } from "react-native-paper";
-
+import { StyleSheet } from "react-native";
+import { database, DATABASE_ID, HABBITS_ID } from "@/lib/appwrite";
+import { Query } from "react-native-appwrite";
+import { useState } from "react";
 export default function Index() {
-  const {signOut} = useAuth()
+  const [habbits,setHabbits] = useState<Habit[]>()
+  const fetchHabbits = async () => {
+    try {
+      const response = await database.listDocuments(DATABASE_ID, HABBITS_ID,[Query.equal("user_id",user?.$id ?? "")]);
+    } catch (error) {
+      console.log("error");
+    }
+  };
+  const { signOut,user } = useAuth();
   return (
     <View
       style={{
@@ -13,9 +24,25 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Link href="/login" style={{width:100, backgroundColor:"red", borderRadius: 5, textAlign:"center",fontSize:16}}> login</Link>
+      <Link
+        href="/login"
+        style={{
+          width: 100,
+          backgroundColor: "red",
+          borderRadius: 5,
+          textAlign: "center",
+          fontSize: 16,
+        }}
+      >
+        {" "}
+        login
+      </Link>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-       <Button mode="text" onPress={signOut} icon={"logout"}> Sign out</Button>
+      <Button mode="text" onPress={signOut} icon={"logout"}>
+        {" "}
+        Sign out
+      </Button>
     </View>
   );
 }
+const styles = StyleSheet.create({});
